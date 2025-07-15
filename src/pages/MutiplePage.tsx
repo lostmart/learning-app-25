d8565044dd274fc4e3a11b669dba438496c97c8e
lostmart <martinpedraza1979@gmail.com>
2025-07-15 21:04:55 +0200

    new starts

---
+++ src/pages/MutiplePage.tsx
@@ -0,0 +1,9 @@
+const MutiplePage = () => {
+	return (
+		<main className="flex min-h-screen flex-col items-center p-2 max-w-xl mx-auto">
+			<h1 className="text-3xl font-bold underline">MutiplePage</h1>
+		</main>
+	)
+}
+
+export default MutiplePage
