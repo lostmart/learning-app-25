e29ed0e15d08ee2ee5fea4328c526aff6a3d4b1a
lostmart <martinpedraza1979@gmail.com>
2025-07-15 22:34:19 +0200

    new

--- src/pages/WriteOptions.tsx
+++ src/pages/WriteOptions.tsx
@@ -1,11 +1,15 @@
 import { useState } from "react"
 import ButtonComp from "../components/ButtonComp"
 import { Link } from "react-router"
+import PointsHolder from "../components/PointsHolder"
+import ExerciseHeader from "../components/ExerciseHeader"
 
 type PhraseState = string[]
 
 const WriteOptions = () => {
 	const [phrase, setPhrase] = useState<PhraseState>([])
+
+	const [points, setPoints] = useState(0)
 
 	const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
 		const target = e.target as HTMLButtonElement
@@ -32,13 +36,13 @@ const WriteOptions = () => {
 				className="flex flex-col items-center p-2 max-w-xl mx-auto"
 				style={{ minHeight: "85vh" }}
 			>
-				<h2 className="text-xl my-2">Write the options right now ...</h2>
-				<img
-					className="rounded-sm mb-2"
-					src="https://images.pexels.com/photos/17918776/pexels-photo-17918776/free-photo-of-dandelion-in-hand.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-					alt="no temas a la noche oscura"
+				<PointsHolder score={points} />
+				<ExerciseHeader
+					className="mb-4 text-center"
+					titleText="Write the options right now ..."
+					imageUrl="https://images.pexels.com/photos/17918776/pexels-photo-17918776/free-photo-of-dandelion-in-hand.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 				/>
-				{phrase.length !== 0 ? <RenderPhrase /> : "No hay palabras todavía"}{" "}
+				{phrase.length !== 0 ? <RenderPhrase /> : ""}{" "}
 				{/* Changed logic to render phrases */}
 				<div className="pt-4 flex gap-2 flex-wrap justify-center">
 					<ButtonComp
