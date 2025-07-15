1696d62efd773d1e2f92c0d0e0d3e0672fd2b68d
lostmart <martinpedraza1979@gmail.com>
2025-07-15 21:27:01 +0200

    new starts

--- src/pages/MutiplePage.tsx
+++ src/pages/MutiplePage.tsx
@@ -1,15 +1,15 @@
 import { useState } from "react"
 
-import ButtonComp from "../components/ButtonComp"
 import { useNavigate } from "react-router"
 import PointsHolder from "../components/PointsHolder"
 import ExerciseHeader from "../components/ExerciseHeader"
+import RenderOptions from "../components/RenderOptions"
 
 const MutiplePage = () => {
 	const navigate = useNavigate()
 	const options = ["En la pizza", "En el parque", "La cancha", "La marmota"]
 	const correctOpt = "En la pizza"
-	let [points, setPoints] = useState(0)
+	const [points, setPoints] = useState(0)
 
 	const handleButtonClick = (option: string) => {
 		console.log("running")
@@ -25,9 +25,10 @@ const MutiplePage = () => {
 			<PointsHolder score={points} />
 			<ExerciseHeader
 				className="mb-4"
-				titleText="Donde sta la casa?"
+				titleText="What is this called ?"
 				imageUrl="https://images.pexels.com/photos/27025471/pexels-photo-27025471/free-photo-of-nourriture-aliments-pizza-restaurant.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 			/>
+			<RenderOptions options={options} handleButtonClick={handleButtonClick} />
 		</main>
 	)
 }
