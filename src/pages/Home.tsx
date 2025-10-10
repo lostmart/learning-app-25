7a18d0e18971f4c3e770150293be5b8b30c3387d
lostmart <martinpedraza1979@gmail.com>
2025-10-10 17:15:55 +0200

    new audios and transitions

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,38 +1,52 @@
 import { Link } from "react-router"
 
 const Home = () => {
+	const linkskArray = [
+		{
+			link: "/video",
+			text: "Video",
+		},
+		{
+			link: "/presentation",
+			text: "Presentation",
+		},
+		{
+			link: "/fillIn",
+			text: "FillIn",
+		},
+		{
+			link: "/mutiple",
+			text: "Multiple",
+		},
+		{
+			link: "/writeOpt",
+			text: "WriteOpt",
+		},
+		{
+			link: "/final",
+			text: "Final",
+		},
+	]
+
 	return (
 		<main
 			className="flex flex-col items-center justify-center"
 			style={{ minHeight: "80vh" }}
 		>
-			<Link to="/video" className="text-2xl uppercase bg-blue-600 p-4">
-				Start !
-			</Link>
-			<Link
-				to="/presentation"
-				className="text-2xl uppercase bg-blue-600 p-4 mt-4"
-			>
-				Presentation
-			</Link>
-			<Link to="/fillIn" className="text-2xl uppercase bg-blue-600 p-4 mt-4">
-				fillIn
-			</Link>
-			<Link to="/mutiple" className="text-2xl uppercase bg-blue-600 p-4 mt-4">
-				Multiple
-			</Link>
-			<Link to="/writeOpt" className="text-2xl uppercase bg-blue-600 p-4 mt-4">
-				WriteOpt
-			</Link>
-			<Link to="/final" className="text-2xl uppercase bg-blue-600 p-4 mt-4">
-				Final
-			</Link>
-			<Link to="/error" className="text-2xl uppercase bg-blue-600 p-4 mt-4">
-				Error
-			</Link>
-			<Link to="/about" className="text-2xl uppercase bg-blue-600 p-4 mt-4">
-				About
-			</Link>
+			<h1 className="text-4xl">Welcome to the presentation</h1>
+			{
+				<>
+					{linkskArray.map((link, i) => (
+						<Link
+							to={link.link}
+							className="text-2xl uppercase bg-blue-600 p-4 mt-4"
+							key={i}
+						>
+							{link.text}
+						</Link>
+					))}
+				</>
+			}
 		</main>
 	)
 }
--- src/pages/Presentation.tsx
+++ src/pages/Presentation.tsx
@@ -4,6 +4,7 @@ import helloAudio from "../assets/hello-one.mp3"
 // import teacerTwo from "../assets/04.png"
 import kid from "../assets/jack.png"
 import AudioPlayButton from "../components/AudioPlayButton"
+import { motion } from "motion/react"
 
 import { Swiper, SwiperSlide } from "swiper/react"
 import { Navigation, Pagination } from "swiper/modules"
@@ -11,19 +12,19 @@ import { Navigation, Pagination } from "swiper/modules"
 import "swiper/swiper-bundle.css"
 
 const Presentation = () => {
-	const [hasPlayed, setHasPlayed] = useState(false)
+	// const [hasPlayed, setHasPlayed] = useState(false)
 
 	// const [slide, setSlide] = useState(0)
 
-	const handlePlay = () => {
-		const audio = new Audio(helloAudio)
-		audio
-			.play()
-			.then(() => setHasPlayed(true))
-			.catch((err) => {
-				console.warn("Playback failed:", err)
-			})
-	}
+	// const handlePlay = () => {
+	// 	const audio = new Audio(helloAudio)
+	// 	audio
+	// 		.play()
+	// 		.then(() => setHasPlayed(true))
+	// 		.catch((err) => {
+	// 			console.warn("Playback failed:", err)
+	// 		})
+	// }
 
 	// const handlePlayTwo = () => {
 	// 	const audio = new Audio(audioTwo)
@@ -47,19 +48,39 @@ const Presentation = () => {
 			>
 				<SwiperSlide>
 					<section className="flex justify-center flex-col items-center p-2 ">
-						<img src={kid} alt="teacher" className="max-w-[460px]" />
-						<div
+						<motion.img
+							initial={{ y: -100, opacity: 0 }}
+							animate={{ y: 0, opacity: 1 }}
+							transition={{
+								duration: 0.5,
+								scale: { type: "spring" },
+							}}
+							src={kid}
+							alt="teacher"
+							className="max-w-[460px]"
+						/>
+						<motion.div
 							className="absolute bottom-10 w-full max-w-[460px]"
 							style={{
 								background: "#ffffffa6",
+							}}
+							initial={{ y: 100, opacity: 0 }}
+							animate={{ y: 0, opacity: 1 }}
+							transition={{
+								duration: 0.6,
+								delay: 0.2,
+								scale: { type: "spring" },
 							}}
 						>
 							<p className="text-center p-2 text-gray-950">
 								Hello. My name is Jack, I’m nine. I’ve got a brother and a
 								sister. This is my favorite computer game. It’s called ‘Roblox’!
 							</p>
-							<AudioPlayButton className="absolute bottom-18 left-2 z-50" audioUrl="https://magenta-fox-373734.hostingersite.com/wp-content/uploads/2025/08/01-jack.mp3" />
-						</div>
+							<AudioPlayButton
+								className="absolute bottom-18 left-2 z-50"
+								audioUrl="https://magenta-fox-373734.hostingersite.com/wp-content/uploads/2025/08/01-jack.mp3"
+							/>
+						</motion.div>
 					</section>
 				</SwiperSlide>
 
