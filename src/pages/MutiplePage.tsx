2ee0840f7733d452d2037f454b15554cbc8f60a8
lostmart <martinpedraza1979@gmail.com>
2025-11-22 22:40:28 +0100

    new words

--- src/pages/MutiplePage.tsx
+++ src/pages/MutiplePage.tsx
@@ -5,17 +5,18 @@ import PointsHolder from "../components/PointsHolder"
 import ExerciseHeader from "../components/ExerciseHeader"
 import RenderOptions from "../components/RenderOptions"
 
-import dadImg from "../assets/01.png"
+import dadImg from "../assets/get-up.png"
 
 const MutiplePage = () => {
 	// const navigate = useNavigate()
 	const options = [
-		"Nice to meet you",
-		"This is my dad",
-		"This is my father",
-		"This is my mom",
+		"Get up",
+		"Wash my face",
+		"Brush my teeth",
+		"Shower",
+		"Go to bed",
 	]
-	const correctOpt = "This is my dad"
+	const correctOpt = "Get up"
 	const [points, setPoints] = useState(0)
 
 	const handleButtonClick = (option: string) => {
@@ -31,7 +32,7 @@ const MutiplePage = () => {
 		<>
 			<PointsHolder score={points} />
 			<main style={{ minHeight: "80vh" }}>
-				<h2 className="text-center">This is my dad</h2>
+				<h2 className="text-center">What is it ?</h2>
 				<div className="flex flex-col items-center p-2 max-w-3xl mx-auto relative md:flex-row md:gap-4">
 					<ExerciseHeader
 						className="mb-4 text-center md:w-[70%]"
