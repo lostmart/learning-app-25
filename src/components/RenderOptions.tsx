e2c71d602b97a1cae8dd10a7d727f5c230d66a46
lostmart <martinpedraza1979@gmail.com>
2025-11-27 21:54:38 +0100

    new styles on modal feedback

--- src/components/RenderOptions.tsx
+++ src/components/RenderOptions.tsx
@@ -8,12 +8,12 @@ type RenderOptProps = {
 const RenderOptions = ({ options, handleButtonClick }: RenderOptProps) => {
 	return (
 		<>
-			{options.map((opt) => {
+			{options.map((opt, i) => {
 				return (
 					<button
 						onClick={() => handleButtonClick(opt.letter)}
-						key={opt.letter}
-						className="w-full p-2 mb-4 bg-blue-500 text-white cursor-pointer"
+						key={i}
+						className={`w-full p-2 mb-4 text-white cursor-pointer hover:bg-blue-500 font-medium bg-blue-${6 + i}00`}
 						type="button"
 						style={{ maxWidth: "560px" }}
 					>
