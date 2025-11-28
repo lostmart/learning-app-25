00c01213a3fe43e80e575a05711357e003877bb2
lostmart <martinpedraza1979@gmail.com>
2025-11-28 08:48:41 +0100

    new error styles

--- src/components/RenderOptions.tsx
+++ src/components/RenderOptions.tsx
@@ -6,6 +6,32 @@ type RenderOptProps = {
 }
 
 const RenderOptions = ({ options, handleButtonClick }: RenderOptProps) => {
+	const colorsArray = [
+		"bg-cyan-700",
+		"bg-blue-600",
+		"bg-indigo-600",
+		"bg-green-600",
+		"bg-yellow-500",
+		"bg-purple-500",
+		"bg-pink-500",
+		"bg-red-500",
+		"bg-orange-500",
+		"bg-amber-500",
+	]
+
+	const hoverColorsArray = [
+		"hover:bg-cyan-800",
+		"hover:bg-blue-800",
+		"hover:bg-indigo-700",
+		"hover:bg-green-700",
+		"hover:bg-yellow-600",
+		"hover:bg-purple-600",
+		"hover:bg-pink-600",
+		"hover:bg-red-600",
+		"hover:bg-orange-600",
+		"hover:bg-amber-600",
+	]
+
 	return (
 		<>
 			{options.map((opt, i) => {
@@ -13,7 +39,7 @@ const RenderOptions = ({ options, handleButtonClick }: RenderOptProps) => {
 					<button
 						onClick={() => handleButtonClick(opt.letter)}
 						key={i}
-						className={`w-full p-2 mb-4 text-white cursor-pointer hover:bg-blue-500 font-medium bg-blue-${6 + i}00`}
+						className={`w-full p-2 mb-4 text-white cursor-pointer ${colorsArray[i]} ${hoverColorsArray[i]} font-medium `}
 						type="button"
 						style={{ maxWidth: "560px" }}
 					>
